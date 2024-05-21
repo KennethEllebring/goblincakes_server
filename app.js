022318e981ef49b07f8a154d0fc99d13e7e8aba6
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2024-05-22 00:23:21 +0200

    change to origins

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://goblincakes.vercel.app/", "https://goblincakes-server.vercel.app/"],
+    origin: ["http://localhost:3000", "https://goblincakes.vercel.app/"],
     credentials: true,
   })
 );
